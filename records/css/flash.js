8cf05c9ba1e37f3afb706e9e5d7c734ab79c796b
vanny <290288575@qq.com>
2017-04-26 21:54:22 +0800

    添加作品蒙古缘

---
+++ records/css/flash.js
@@ -0,0 +1,81 @@
+var g_jquery_ready = 0;
+function pageMain()
+{
+	if(g_jquery_ready == 1)
+	{
+		$(".cd").addClass("flash");
+	}
+	else
+	{
+		$(document).ready(function(){
+			g_jquery_ready = 1;
+			$(".cd").addClass("flash");
+		});
+	}
+}
+function pageOut()
+{
+	if(g_jquery_ready == 1)
+	{
+		$(".cd").removeClass("flash");
+	}
+}
+
+// 添加删除class
+$(function() {
+    $(".index .pop_bt").click(function () {
+        if ($(".index").hasClass("on")) {
+            $(".index").addClass("off").removeClass("on")
+        } else {
+            $(".index").addClass("on").removeClass("off")
+        }
+    });
+});
+
+$(function() {
+    $(".list .pop_bt").click(function () {
+        if ($(".list").hasClass("on")) {
+            $(".list").addClass("off").removeClass("on")
+        } else {
+            $(".list").addClass("on").removeClass("off")
+        }
+    });
+});
+
+$(function() {
+    $(".bnt").click(function () {
+        if ($(".list").hasClass("en")) {
+            $(".list").addClass("zh").removeClass("en");
+            $(".zhongwen").show();
+            $(".english").hide();
+
+        } else {
+            $(".list").addClass("en").removeClass("zh");
+            $(".zhongwen").hide();
+            $(".english").show();
+        }
+    });
+});
+
+
+
+$(function() {
+    $(".info .pop_bt").click(function () {
+        if ($(".info").hasClass("on")) {
+            $(".info").addClass("off").removeClass("on")
+        } else {
+            $(".info").addClass("on").removeClass("off")
+        }
+    });
+});
+
+$(function() {
+    $(".pg .pop_bt").click(function () {
+        if ($(".pg").hasClass("on")) {
+            $(".pg").addClass("off").removeClass("on")
+        } else {
+            $(".pg").addClass("on").removeClass("off")
+        }
+    });
+});
+pageMain();
